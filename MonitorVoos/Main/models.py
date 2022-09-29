62e73f302c650003e2228135837703f9848e1889
Pedro Viveiros <pedrohr.viveiros@gmail.com>
2022-09-29 16:42:01 -0300

    Criação dos princípios de Models

--- MonitorVoos/Main/models.py
+++ MonitorVoos/Main/models.py
@@ -1,3 +1,33 @@
 from django.db import models
 
 # Create your models here.
+class Flight(models.Model):
+    pass
+
+
+# TODO: confirmar necessidade de classes de relatorio
+class SpecificFlightReport(models.Model):
+    pass
+
+
+# TODO: confirmar necessidade de classes de relatorio
+class StatusReport(models.Model):
+    pass
+
+
+class Pilot(models.Model):
+    pass
+
+
+class Plane(models.Model):
+    pass
+
+
+class User(models.Model):
+    name = models.CharField()
+    document = models.PositiveIntegerField()
+    email = models.EmailField()
+
+
+class Airport(models.Model):
+    pass
