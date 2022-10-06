7744e62497bb846ef2b8767904f7a18389f6838c
Michel <michelllbrito@hotmail.com>
2022-10-06 16:35:28 -0300

    Ajustes

--- MonitorVoos/Main/models.py
+++ MonitorVoos/Main/models.py
@@ -1,47 +1,47 @@
+from email.policy import default
 from django.db import models
 from django.db.models import SET_NULL, CASCADE
 
 
-class FlightStatus(models.IntegerChoices):
-    REGISTERED = 1, "Cadastrado"
-    BOARDING = 2, "Embarcando"
-    PROGRAMMED = 3, "Programado"
-    TAXIING = 4, "Taxiando"
-    READY = 5, "Pronto"
-    AUTHORIZED = 6, "Autorizado"
-    ON_AIR = 7, "Em voo"
-    LANDED = 8, "Aterrissado"
-    CANCELLED = 9, "Cancelado"
-
-    class Meta:
-        db_table = "status"
-
-
 class Pilot(models.Model):
-    name = models.CharField(max_lenght=256)
-    anac_code = models.PositiveIntegerField(max_length=6)
-    cpf = models.PositiveIntegerField(max_length=11)
+    name = models.CharField(max_length=256)
+    anac_code = models.CharField(max_length=6)
+    cpf = models.CharField(max_length=11)
 
     class Meta:
         db_table = "pilots"
 
 
 class User(models.Model):
-    name = models.CharField(max_lenght=256)
-    cpf = models.PositiveIntegerField(max_lenght=11)
+    name = models.CharField(max_length=256)
+    cpf = models.CharField(max_length=11)
     email = models.EmailField()
-    password = models.CharField()
+    password = models.CharField(max_length=256)
 
     class Meta:
         db_table = "users"
 
 
 class Airport(models.Model):
-    icao = models.CharField(max_lenght=4)
-    name = models.CharField(max_lenght=256)
-    city = models.CharField(max_lenght=256)
-    state = models.CharField(max_lenght=256)
-    country = models.CharField(max_lenght=256)
+    icao = models.CharField(max_length=4)
+    name = models.CharField(max_length=256)
+    city = models.CharField(max_length=256)
+    state = models.CharField(max_length=256)
+    country = models.CharField(max_length=256)
 
     class Meta:
         db_table = "airports"
+
+
+class Flight(models.Model):
+    pilot = models.ForeignKey(Pilot, on_delete=SET_NULL, blank=True, null=True, related_name='pilots')
+    origin_airport = models.ForeignKey(Airport, on_delete=CASCADE, related_name='origin_airports')
+    destination_airport = models.ForeignKey(Airport, on_delete=CASCADE, related_name='destination_airports')
+    status = models.CharField(default='Cadastrado', max_length=256)
+    estimated_departure = models.DateTimeField()
+    estimated_arrival = models.DateTimeField()
+    real_departure = models.DateTimeField(blank=True, null=True)
+    real_arrival = models.DateTimeField(blank=True, null=True)
+
+    class Meta:
+        db_table = 'flights'
