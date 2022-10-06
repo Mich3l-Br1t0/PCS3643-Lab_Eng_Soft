eaa6d5d5396ee100d26cd3f5ec74a190fa1af29a
Pedro Viveiros <pedrohr.viveiros@gmail.com>
2022-10-06 15:34:35 -0300

    [PR002] Criação de atributos no aeroporto

--- MonitorVoos/Main/models.py
+++ MonitorVoos/Main/models.py
@@ -2,6 +2,20 @@ from django.db import models
 
 
 # Create your models here.
+
+
+class FlightStatus(models.IntegerChoices):
+    REGISTERED = 1, "Cadastrado"
+    BOARDING = 2, "Embarcando"
+    PROGRAMMED = 3, "Programado"
+    TAXIING = 4, "Taxiando"
+    READY = 5, "Pronto"
+    AUTHORIZED = 6, "Autorizado"
+    ON_AIR = 7, "Em voo"
+    LANDED = 8, "Aterrissado"
+    CANCELLED = 9, "Cancelado"
+
+
 class Flight(models.Model):
     pass
 
@@ -25,22 +39,18 @@ class Plane(models.Model):
 
 
 class User(models.Model):
-    name = models.CharField()
-    document = models.PositiveIntegerField()
+    name = models.CharField(max_lenght=256)
+    cpf = models.PositiveIntegerField(max_lenght=11)
     email = models.EmailField()
+    password = models.CharField()
 
 
 class Airport(models.Model):
-    pass
+    icao = models.CharField(max_lenght=4)
+    name = models.CharField(max_lenght=256)
+    city = models.CharField(max_lenght=256)
+    state = models.CharField(max_lenght=256)
+    country = models.CharField(max_lenght=256)
 
-
-class FlightStatus(models.IntegerChoices):
-    REGISTERED = 1, "Cadastrado"
-    BOARDING = 2, "Embarcando"
-    PROGRAMMED = 3, "Programado"
-    TAXIING = 4, "Taxiando"
-    READY = 5, "Pronto"
-    AUTHORIZED = 6, "Autorizado"
-    ON_AIR = 7, "Em voo"
-    LANDED = 8, "Aterrissado"
-    CANCELLED = 9, "Cancelado"
+    class Meta:
+        db_table = "airports"
