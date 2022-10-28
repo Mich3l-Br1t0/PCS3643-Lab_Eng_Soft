216f81f03af277fb09c76fe8e55d72a51ac2368b
Michel <michelllbrito@hotmail.com>
2022-10-28 15:10:49 -0300

    Ajuste na organização e criação do registro de user

--- MonitorVoos/Main/urls.py
+++ MonitorVoos/Main/urls.py
@@ -4,12 +4,7 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("home", views.home, name="home"),
-    path("signup", views.signup, name="signup"),
-    #path("", index),
-    #path("home/", home),
-    #path("signup/", signup),
-    #path("home/reports/", reports),
-    #path("home/monitoring/", monitoring),
-    #path("home/crud/", crud),
+    path("home/", views.home, name="home"),
+    path("signup/", views.signup, name="signup"),
+    path("register/", views.register, name="register")
 ]
--- MonitorVoos/Main/views.py
+++ MonitorVoos/Main/views.py
@@ -1,7 +1,21 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, authenticate
+from .forms import Registerform
+from django.views.decorators.csrf import csrf_exempt
 
 
-def index(request):        
+def register(request):
+    if request.method == "POST":
+        form = Registerform(request.POST)
+        if form.is_valid():
+            form.save()
+        return redirect("")
+    else:
+        form = Registerform()
+    return render(request, "register.html", {"form": form})
+
+
+def index(request):
     return render(request, "index.html")
 
 
