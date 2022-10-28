e89e919fd2c40f6fb21194927bde4c4c0bdf0fc9
Victor de Almeida Santana <80405995+VictorASantana@users.noreply.github.com>
2022-10-27 21:43:39 -0300

    Adding "creating flight" base config

---
+++ MonitorVoos/static/js/crud.js
@@ -0,0 +1,7 @@
+function createNew() {
+    Screen = document.getElementById("second").style.visibility = "visible";
+}
+
+function submit() {
+    Screen = document.getElementById("second").style.visibility = "hidden";
+}
