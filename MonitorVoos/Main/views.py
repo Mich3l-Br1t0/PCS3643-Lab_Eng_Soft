e43752c7d049ba6013b6dd4959cc07a20175d64e
Victor de Almeida Santana <80405995+VictorASantana@users.noreply.github.com>
2022-11-03 14:22:28 -0300

    adding import

--- MonitorVoos/Main/views.py
+++ MonitorVoos/Main/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import authenticate
-from .forms import RegisterForm
+from .forms import RegisterForm, Newflightform
 from Main.models import User_data
 
 
