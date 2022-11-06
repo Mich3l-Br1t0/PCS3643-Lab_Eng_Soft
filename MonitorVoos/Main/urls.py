f1dc8cf47b3cfdb3c478f1fa54b8b1006ccccdb1
Victor de Almeida Santana <80405995+VictorASantana@users.noreply.github.com>
2022-11-06 15:25:10 -0300

    Adicionando ícones e corrigindo a navegação

--- MonitorVoos/Main/urls.py
+++ MonitorVoos/Main/urls.py
@@ -8,4 +8,6 @@ urlpatterns = [
     path("signup/", views.signup, name="signup"),
     path("home/flights_crud/", views.flights_crud, name="flights_crud"),
     path("home/airline_crud/", views.airline_crud, name="airline_crud"),
+    path("home/reports", views.reports, name="reports"),
+    path("home/monitoring", views.monitoring, name="monitoring"),
 ]
