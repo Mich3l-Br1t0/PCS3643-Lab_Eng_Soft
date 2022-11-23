bcae4bfca58bb7344881db4c2e006e0c4d010689
Victor de Almeida Santana <80405995+VictorASantana@users.noreply.github.com>
2022-11-23 07:59:33 -0300

    Adicionando opção voo chegando ou partindo

--- MonitorVoos/Main/forms.py
+++ MonitorVoos/Main/forms.py
@@ -21,6 +21,11 @@ STATUS_CHOICES = [
     ("Aguardando Desembarque", "Aguardando Desembarque"),
 ]
 
+DEPARTURE_CHOICES = [
+    (True, "Partida"),
+    (False, "Chegada"),
+]
+
 
 class RegisterForm(UserCreationForm):
     first_name = forms.CharField(max_length=100, label="Nome")
@@ -55,6 +60,10 @@ class Newflightform(ModelForm):
         label="Status", widget=forms.Select(choices=STATUS_CHOICES)
     )
 
+    is_departure_flight = forms.BooleanField(
+        label="Chegada ou saida", widget=forms.Select(choices=DEPARTURE_CHOICES)
+    )
+
     class Meta:
         model = Flight
         fields = [
@@ -65,6 +74,7 @@ class Newflightform(ModelForm):
             "destination_airport",
             "airline",
             "status",
+            "is_departure_flight",
         ]
         labels = {
             "estimated_departure": "Partida Estimada (AAAA-MM-DD hh:mm)",
@@ -81,12 +91,12 @@ class Newflightform(ModelForm):
         destination_airport = cleaned_data.get("destination_airport")
         if origin_airport == destination_airport:
             raise ValidationError(
-                "Aeroporto de destino não pode ser igual o de partida")
+                "Aeroporto de destino não pode ser igual o de partida"
+            )
         estimated_departure = cleaned_data.get("estimated_departure")
         estimated_arrival = cleaned_data.get("estimated_arrival")
         if estimated_departure > estimated_arrival:
-            raise ValidationError(
-                "Partida estimada não pode ser maior que chegada")
+            raise ValidationError("Partida estimada não pode ser maior que chegada")
         return cleaned_data
 
 
@@ -115,12 +125,11 @@ class AirportForm(forms.ModelForm):
 
 
 class ReportForm(forms.Form):
-    start_date = forms.DateField(
-        label='Data início', widget=forms.SelectDateWidget())
-    end_date = forms.DateField(
-        label='Data fim', widget=forms.SelectDateWidget())
+    start_date = forms.DateField(label="Data início", widget=forms.SelectDateWidget())
+    end_date = forms.DateField(label="Data fim", widget=forms.SelectDateWidget())
     Airline = forms.ModelChoiceField(
-        queryset=Airline.objects.all(), required=False, label="Companhia Aérea")
+        queryset=Airline.objects.all(), required=False, label="Companhia Aérea"
+    )
     # Todos os voos do sistema num período
     # Todos os voos de uma companhia aérea num período + detalhes
 
@@ -130,5 +139,6 @@ class ReportForm(forms.Form):
         end_date = cleaned_data.get("end_date")
         if start_date >= end_date:
             raise ValidationError(
-                "Data de início não pode ser maior ou igual data de fim")
+                "Data de início não pode ser maior ou igual data de fim"
+            )
         return cleaned_data
