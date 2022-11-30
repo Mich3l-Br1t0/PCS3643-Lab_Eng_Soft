350dffba4e0b09125f6c0f51295ddf11ca1b92fc
Pedro Viveiros <pedrohr.viveiros@gmail.com>
2022-11-30 00:45:12 -0300

    [PR027] Parametrização de variaveis comuns

---
+++ MonitorVoos/Main/commons.py
@@ -0,0 +1,19 @@
+STATUS_CHOICES = [
+    ("registered", "Cadastrado"),
+    ("boarding", "Embarcando"),
+    ("cancelled", "Cancelado"),
+    ("scheduled", "Programado"),
+    ("taxiing", "Taxiando"),
+    ("ready", "Pronto"),
+    ("authorized", "Autorizado"),
+    ("flying", "Em voo"),
+    ("landed", "Aterrissado"),
+]
+
+PROFESSION_CHOICES = [
+    ("Manager", "Gerente de Operações"),
+    ("Control", "Torre de Controle"),
+    ("Pilot", "Piloto"),
+    ("Worker", "Funcionário da Companhia Aérea"),
+    ("Operator", "Operador de voo"),
+]
